0db3432c6df2af19fc4e3dbee77453050c8521c6
Wig Cheng <wigcheng@ieiworld.com>
2023-04-21 13:33:20 +0800

    ComportTest: implement RS232 loopback testing
    Add SerialPort JNI based library first, and load this libaray to
    implement loopback function in the test.

--- app/src/main/java/com/ayst/stresstest/test/comport/ComportTestFragment.java
+++ app/src/main/java/com/ayst/stresstest/test/comport/ComportTestFragment.java
@@ -25,17 +25,104 @@ import android.view.ViewGroup;
 import androidx.annotation.Nullable;
 
 import com.ayst.stresstest.R;
-import com.ayst.stresstest.test.base.BaseTimingTestFragment;
+import com.ayst.stresstest.test.base.BaseCountTestWithTimerFragment;
 import com.ayst.stresstest.test.base.TestType;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.InvalidParameterException;
+import java.util.Objects;
 
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import android.util.Log;
+import com.ayst.stresstest.test.comport.SerialPort;
+import com.ayst.stresstest.util.NetworkUtils;
 
-public class ComportTestFragment extends BaseTimingTestFragment {
+public class ComportTestFragment extends BaseCountTestWithTimerFragment {
 
     Unbinder unbinder;
 
     private String mPath;
+
+    protected SerialPort mSerialPort;
+    protected OutputStream mOutputStream;
+    private InputStream mInputStream;
+    private ComportTestFragment.ReadThread mReadThread;
+
+    byte mValueToSend;
+    boolean mByteReceivedBack;
+    boolean mTestReturn = true;
+    Object mByteReceivedBackSemaphore = new Object();
+    Integer mIncoming = new Integer(0);
+    Integer mOutgoing = new Integer(0);
+    Integer mLost = new Integer(0);
+    Integer mCorrupted = new Integer(0);
+    Integer mFailedRecord = new Integer(0);
+
+    SendingThread mSendingThread;
+
+    private class SendingThread extends Thread {
+        @Override
+        public void run() {
+            while (!isInterrupted()) {
+                synchronized (mByteReceivedBackSemaphore) {
+                    mByteReceivedBack = false;
+                    try {
+                        if (mOutputStream != null) {
+                            mOutputStream.write(mValueToSend);
+                        } else {
+                            return;
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        return;
+                    }
+                    mOutgoing++;
+                    // Wait for 1000ms before sending next byte, or as soon as
+                    // the sent byte has been read back.
+                    try {
+                        mByteReceivedBackSemaphore.wait(5000);
+                        if (mByteReceivedBack == true) {
+                            // Byte has been received
+                            mIncoming++;
+                        } else {
+                            // Timeout
+                            mLost++;
+                            Log.d(TAG,"Wig failed");
+                        }
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+        }
+    }
+
+    protected class ReadThread extends Thread {
+
+        @Override
+        public void run() {
+            super.run();
+            while(!isInterrupted()) {
+                int size;
+                try {
+                    byte[] buffer = new byte[64];
+                    if (mInputStream == null) return;
+                    size = mInputStream.read(buffer);
+                    if (size > 0) {
+                        onDataReceived(buffer, size);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return;
+                }
+            }
+        }
+    }
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -75,10 +162,28 @@ public class ComportTestFragment extends BaseTimingTestFragment {
 
     @Override
     public void onStartClicked() {
+        if (mSerialPort == null) {
+            try {
+                mSerialPort = new SerialPort(new File("/dev/ttyUSB0"), 115200, 0);
+                mOutputStream = mSerialPort.getOutputStream();
+                mInputStream = mSerialPort.getInputStream();
+
+                /* Create a receiving thread */
+                mReadThread = new ComportTestFragment.ReadThread();
+                mReadThread.start();
+                mSendingThread = new SendingThread();
+                mSendingThread.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        super.onStartClicked();
     }
 
     @Override
     public void start() {
+        super.start();
     }
 
     @Override
@@ -87,8 +192,37 @@ public class ComportTestFragment extends BaseTimingTestFragment {
     }
 
     @Override
+    protected boolean testOnce() {
+        if (Objects.equals(mFailedRecord, mLost))
+            mTestReturn = true;
+        else {
+            mTestReturn = false;
+            mFailedRecord = mLost;
+        }
+        return mTestReturn;
+    }
+
+    @Override
     public boolean isSupport() {
         return true;
+    }
+
+    protected void onDataReceived(byte[] buffer, int size) {
+        synchronized (mByteReceivedBackSemaphore) {
+            int i;
+            for (i = 0; i < size; i++) {
+                if ((buffer[i] == mValueToSend) && (mByteReceivedBack == false)) {
+                    mValueToSend++;
+                    // This byte was expected
+                    // Wake-up the sending thread
+                    mByteReceivedBack = true;
+                    mByteReceivedBackSemaphore.notify();
+                } else {
+                    // The byte was not expected
+                    mCorrupted++;
+                }
+            }
+        }
     }
 
     @Override
